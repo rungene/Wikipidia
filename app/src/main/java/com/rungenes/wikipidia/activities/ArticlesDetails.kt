c411835900a23975847ea4480397cb3aababad49
rungene <rungenesamuel@gmail.com>
2019-07-17 21:27:50 +0300

    Initial commit

---
+++ app/src/main/java/com/rungenes/wikipidia/activities/ArticlesDetails.kt
@@ -0,0 +1,19 @@
+package com.rungenes.wikipidia.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.rungenes.wikipidia.R
+
+class ArticlesDetails: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.details_articles)
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/rungenes/wikipidia/activities/Search.kt
@@ -0,0 +1,13 @@
+package com.rungenes.wikipidia.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.rungenes.wikipidia.R
+
+class Search : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
